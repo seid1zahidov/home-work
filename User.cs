3f33df546053fdb7e64eb28474a06399a89a0031
seid1zahidov <saidiz@code.edu.az>
2022-04-11 17:00:40 -0700

    Seidjop

---
+++ User.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp35
+{
+    class User
+    {
+        public string _userName;
+        public string _password;
+
+        public string Username 
+        {
+            get 
+            {
+                return _userName;
+
+                
+                
+            }
+            set 
+            {
+                if (value.Length >= 8)
+                {
+                    _userName = Username;
+                }
+                else
+                {
+                    Console.WriteLine("Duzgun daxil et");
+                }
+            
+            
+            
+            }
+            
+        
+        
+        }
+        public string Password 
+        {
+            get 
+            {
+                return _password;
+            
+            }
+            set 
+            {
+                if (value.Length >= 8)
+                {
+                    _password = Password;
+
+                    foreach (char item in _password)
+                    {
+                        if (char.IsUpper(item))
+                        {
+
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("duzgun daxil edin");
+                }
+            
+            
+            }
+        
+        
+        
+        }
+    }
+}
